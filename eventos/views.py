1745eb4922485df83a4e63a9054fb496db7adb45
MatiiBasualdo <lavaronics10@hotmail.com>
2017-07-14 08:08:56 -0300

    Agregado un comentario en views

--- eventos/views.py
+++ eventos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Productos
 
+# Una prueba
+
 def listar_productos(request):
     productos = Productos.objects.all()
     return render(request, 'eventos/productos.html', {'productos': productos})
